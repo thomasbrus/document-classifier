04caf6306e1a7ea9a25536086791aac3a02f5491
Thomas Brus <thomas.brus@me.com>
2015-09-26 13:01:56 +0200

    Added #vocabulary_size and refactored classifier

--- lib/document_classifier.rb
+++ lib/document_classifier.rb
@@ -12,24 +12,26 @@ class DocumentClassifier
   attr_accessor :ignore_words, :stop_words
 
   def initialize(categories)
-    @categories, @word_frequencies = categories, categories.reduce({}) do |hsh, key|
-      hsh[key] = Hash.new(0)
-      hsh
+    @categories, @ignore_words, @stop_words = categories, [], []
+
+    @frequencies = categories.reduce({}) do |word_frequencies, key|
+      word_frequencies[key] = Hash.new(0)
+      word_frequencies
     end
 
     yield(self) if block_given?
   end
 
   def train(category, text)
-    tokenize(text) { |word| @word_frequencies.fetch(category)[word] += 1 }
-    flush_cache(:total_number_of_words, :number_of_words)
+    tokenize(text) { |word| @frequencies.fetch(category)[word] += 1 }
+    flush_cache(:vocabulary_size, :number_of_words, :total_number_of_words)
   end
 
   def classify(text)
     categories.max_by do |category|
       tokenize(text).reduce(0) do |total, word|
         a = frequency(category, word) + smoothing_options.fetch(:k)
-        b = number_of_words(category) + smoothing_options.fetch(:k) * total_number_of_words
+        b = number_of_words(category) + smoothing_options.fetch(:k) * vocabulary_size
         total + Math.log2(Rational(a, b).to_f)
       end
     end
@@ -40,23 +42,31 @@ class DocumentClassifier
   end
 
   private def frequency(category, word)
-    @word_frequencies.fetch(category)[word]
+    @frequencies.fetch(category)[word]
+  end
+
+  private memoize def vocabulary_size
+    categories.reduce(0) do |count, category|
+      count + @frequencies.fetch(category).count
+    end
+  end
+
+  private memoize def number_of_words(category)
+    @frequencies.fetch(category).values.reduce(:+)
   end
 
   private memoize def total_number_of_words
     categories.reduce(0) { |count, category| count + number_of_words(category) }
   end
 
-  private memoize def number_of_words(category)
-    @word_frequencies.fetch(category).values.reduce(:+)
+  private def tokenize(text, &block)
+    tokenizer(text).each_word(&block)
   end
 
-  private def tokenize(text, &block)
-    tokenizer = Tokenizer.new(text) do |tokenizer_config|
-      tokenizer_config.ignore_words = ignore_words unless ignore_words.nil?
-      tokenizer_config.stop_words = stop_words unless ignore_words.nil?
+  private def tokenizer(text)
+    Tokenizer.new(text) do |config|
+      config.ignore_words = ignore_words
+      config.stop_words = stop_words
     end
-
-    tokenizer.each_word(&block)
   end
 end
